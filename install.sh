cc225a3853114cafbe2b8ead281587aeefa361ca
Tamás Barta <barta.tamas.d@gmail.com>
2018-12-05 00:10:20 +0100

    Add install script and tmux

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOTFILES_HOME=$HOME/.dotfiles
+REPOSITORY=https://gitlab.com/TamasBarta/dotfiles.git
+
+git clone $REPOSITORY $DOTFILES_HOME
+
+$DOTFILES_HOME/zsh/fzf/install --bin
+
+[ -f $HOME/.zshrc ] && echo "Backing up previous .zshrc as .zshrc.bak" && mv $HOME/.zshrc{,.bak}
+[ -f $HOME/.tmux.conf ] && echo "Backing up previous .tmux.conf as .tmux.conf.bak" && mv $HOME/.tmux.conf{,.bak}
+
+ln -s $DOTFILES_HOME/zsh/.zshrc $HOME/.zshrc
+ln -s $DOTFILES_HOME/tmux/.tmux.conf $HOME/.tmux.conf
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -4,6 +4,7 @@ export DOTFILES_HOME=$HOME/.dotfiles
 
 source $DOTFILES_HOME/zsh/oh-my-zsh.zsh
 source $DOTFILES_HOME/zsh/path.zsh
+source $DOTFILES_HOME/zsh/fzf.zsh
 source $DOTFILES_HOME/zsh/aliases.zsh
 source $DOTFILES_HOME/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $DOTFILES_HOME/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
