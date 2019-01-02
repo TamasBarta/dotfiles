5a6532677bd289c057bdf6770a88f4f77c2e317d
Tamás Barta <barta.tamas.d@gmail.com>
2019-01-02 11:46:57 +0100

    Add Vundle

--- install.sh
+++ install.sh
@@ -10,7 +10,11 @@ $DOTFILES_HOME/zsh/fzf/install --bin
 [ -f $HOME/.zshrc ] && echo "Backing up previous .zshrc as .zshrc.bak" && mv $HOME/.zshrc{,.bak}
 [ -f $HOME/.tmux.conf ] && echo "Backing up previous .tmux.conf as .tmux.conf.bak" && mv $HOME/.tmux.conf{,.bak}
 [ -f $HOME/.vimrc ] && echo "Backing up previous .vimrc as .vimrc.bak" && mv $HOME/.vimrc{,.bak}
+[ -f $HOME/.vim ] && echo "Backing up previous .vim as .vim.bak" && mv $HOME/.vim{,.bak}
 
 ln -s $DOTFILES_HOME/zsh/.zshrc $HOME/.zshrc
 ln -s $DOTFILES_HOME/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $DOTFILES_HOME/vim/.vimrc $HOME/.vimrc
+ln -s $DOTFILES_HOME/vim/.vim $HOME/.vim
+
+vim +PluginInstall +qall
