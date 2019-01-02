82a14cffa116ada4a6367430b51cf56b7846226c
Tamás Barta <barta.tamas.d@gmail.com>
2019-01-02 11:13:57 +0100

    Replace hardcoded home with $HOME

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,13 +1,13 @@
 # Setting up the path
 
-export GOPATH=/home/tamas/.gopath
+export GOPATH=$HOME/.gopath
 export PATH="$PATH:${GOPATH//://bin:}/bin"
 
-PATH="$PATH:/home/tamas/.config/variety/scripts"
-PATH="$PATH:/home/tamas/.apps/scripts"
-PATH="$PATH:/home/tamas/.gem/ruby/2.4.0/bin"
+PATH="$PATH:$HOME/.config/variety/scripts"
+PATH="$PATH:$HOME/.apps/scripts"
+PATH="$PATH:$HOME/.gem/ruby/2.4.0/bin"
 
-# export PATH="/home/tamas/.phpbrew/php/php-5.5.24/bin:/home/tamas/.phpbrew/bin:/usr/local/heroku/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/tamas/.apps/adt-bundle-linux-x86_64-20140321/sdk/platform-tools:/home/tamas/.apps/adt-bundle-linux-x86_64-20140321/sdk/tools:/home/tamas/.composer/vendor/bin:/home/tamas/.apps/scripts"
+# export PATH="$HOME/.phpbrew/php/php-5.5.24/bin:$HOME/.phpbrew/bin:/usr/local/heroku/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$HOME/.apps/adt-bundle-linux-x86_64-20140321/sdk/platform-tools:$HOME/.apps/adt-bundle-linux-x86_64-20140321/sdk/tools:$HOME/.composer/vendor/bin:$HOME/.apps/scripts"
 export PATH=$HOME/.nodebrew/current/bin:$PATH
 export PATH=$HOME/Android/Sdk/platform-tools:$PATH
 
