2bb3189b720f182e56c2ef46d746e9d429ba3f91
Tamás Barta <barta.tamas.d@gmail.com>
2019-01-03 17:27:41 +0100

    Add gitconfig

--- install.sh
+++ install.sh
@@ -11,10 +11,12 @@ $DOTFILES_HOME/zsh/fzf/install --bin
 [ -f $HOME/.tmux.conf ] && echo "Backing up previous .tmux.conf as .tmux.conf.bak" && mv $HOME/.tmux.conf{,.bak}
 [ -f $HOME/.vimrc ] && echo "Backing up previous .vimrc as .vimrc.bak" && mv $HOME/.vimrc{,.bak}
 [ -f $HOME/.vim ] && echo "Backing up previous .vim as .vim.bak" && mv $HOME/.vim{,.bak}
+[ -f $HOME/.gitconfig ] && echo "Backing up previous .gitconfig as .gitconfig.bak" && mv $HOME/.gitconfig{,.bak}
 
 ln -s $DOTFILES_HOME/zsh/.zshrc $HOME/.zshrc
 ln -s $DOTFILES_HOME/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $DOTFILES_HOME/vim/.vimrc $HOME/.vimrc
 ln -s $DOTFILES_HOME/vim/.vim $HOME/.vim
+ln -s $DOTFILES_HOME/git/.gitconfig $HOME/.gitconfig
 
 vim +PluginInstall +qall
