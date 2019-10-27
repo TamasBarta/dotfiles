73960d9b29ee9e72c5cbcb98873bbca1956d3e8b
Tamás Barta <barta.tamas.d@gmail.com>
2019-10-27 02:43:45 +0100

    Add missing quotes

--- install/macos.sh
+++ install/macos.sh
@@ -11,7 +11,7 @@ ln -s $DOTFILES_HOME/karabiner $HOME/.config/
 ln -s $DOTFILES_HOME/yabai/.yabairc $DOTFILES_HOME/yabai/.skhdrc $HOME/
 
 # Set up Übersicht
-mkdir -p $HOME/Library/Application Support/Übersicht/widgets
+mkdir -p "$HOME/Library/Application Support/Übersicht/widgets"
 git clone https://github.com/zzzeyez/pecan.git "$HOME/Library/Application Support/Übersicht/widgets/pecan"
 ln -s "${HOME}/Library/Application Support/Übersicht/widgets/pecan/pecan-style" "/usr/local/bin/pecan-style"
 
