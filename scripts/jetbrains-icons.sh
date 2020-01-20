30602ae21088563954d88c9228e865dbd297c266
Tamás Barta <barta.tamas.d@gmail.com>
2020-01-20 02:41:21 +0100

    Add CLion to icon fixer script

--- scripts/jetbrains-icons.sh
+++ scripts/jetbrains-icons.sh
@@ -2,3 +2,4 @@
 
 sed -i 's/Icon=.*/Icon=android-studio/' $HOME/.local/share/applications/jetbrains-studio.desktop
 sed -i 's/Icon=.*/Icon=intellij-idea/' $HOME/.local/share/applications/jetbrains-idea.desktop
+sed -i 's/Icon=.*/Icon=clion/' $HOME/.local/share/applications/jetbrains-clion.desktop
