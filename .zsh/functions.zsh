8a9f8e9c7acc51a8ee19f1cee0f48d3a40d35914
Tamás Barta <barta.tamas.d@gmail.com>
2020-05-29 14:31:29 +0200

    Update Karabiner config

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -2,3 +2,5 @@ function akill() {
     adb shell ps | grep $1 | grep -Eo " [0-9]+ " | head -1 | xargs adb shell kill
 }
 
+function javaDiff() { diff <(git diff "$@" -G "^import ") <(git diff "$@") | sed "s/^> //" | vim - }
+
--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -7,5 +7,9 @@ export GOPATH=$HOME/.gopath
 export PATH="$PATH:${GOPATH//://bin:}/bin"
 
 # Proguard
+export PATH=$PATH:$HOME/Library/Android/sdk/tools/proguard/bin
+export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
+export PATH=$PATH:$HOME/Library/Android/sdk/tools
+export PATH=$PATH:$HOME/Library/Android/sdk/tools/bin
 export PATH=$PATH:/opt/android-sdk/tools/proguard/bin
 export PATH=$PATH:/opt/android-sdk/platform-tools
