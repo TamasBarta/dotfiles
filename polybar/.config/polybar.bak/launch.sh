c000dd3acb00bde86f03833cc1e3dd28c9a32ea0
Tamás Barta <barta.tamas.d@gmail.com>
2020-12-27 19:16:49 +0100

    Change Polybar settings

--- polybar/.config/polybar.bak/launch.sh
+++ polybar/.config/polybar.bak/launch.sh
@@ -4,11 +4,10 @@
 
 # Terminate already running bar instances
 killall -q polybar
-pkill -f playerctl\ --player=spotify\ metadata\ --format\ 
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
-# polybar mainleft -c ~/.config/polybar/config.ini &
+polybar -c ~/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config.ini secondary &
