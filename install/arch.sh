99735810d352ffc02d85815a65eb398b122c0659
Tamás Barta <barta.tamas.d@gmail.com>
2021-03-18 11:43:26 +0100

    Add desktop changes

--- install/arch.sh
+++ install/arch.sh
@@ -1,7 +1,7 @@
 # Install packages
 
 sudo pacman -Suyy
-sudo pacman -S kitty nvim tmux zsh unzip zip stow xautomation efibootmgr bspwm sxhkd picom lxappearance qt5ct wmname dunst xcape
+sudo pacman -S kitty nvim tmux zsh unzip zip stow xautomation efibootmgr bspwm sxhkd picom lxappearance qt5ct wmname dunst xcape base-devel go
 
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 cd /tmp/yay
