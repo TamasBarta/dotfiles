927fae234d3cffc17cb8fd064deff1d95d9de458
Tamás Barta <barta.tamas.d@gmail.com>
2021-04-28 17:24:57 +0200

    Make scripts non interactive

--- install/arch.sh
+++ install/arch.sh
@@ -1,6 +1,6 @@
 # Install packages
 
-sudo pacman -Suyy git
+sudo pacman --noconfirm -Suyy git go
 
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 cd /tmp/yay
@@ -9,16 +9,16 @@ cd -
 rm -rf /tmp/yay
 
 # common
-yay -S kitty nvim tmux zsh unzip zip stow efibootmgr qt5ct wmname dunst base-devel go adapta-gtk-theme network-manager-applet htop filelight kvantum-qt5 playerctl libratbag telegram-desktop spotify spotifywm-git bitwarden-bin bc rofi-lbonn-wayland-git jetbrains-toolbox fzf nerd-fonts-iosevka ttf-iosevka-aile insync papirus-icon-theme pulseaudio bluez pavucontrol paprefs android-tools android-udev android-sdk spicetify-cli spicetify-themes-git brightnessctl
+yay --noconfirm -S kitty nvim tmux zsh unzip zip stow efibootmgr qt5ct wmname dunst base-devel go adapta-gtk-theme network-manager-applet htop filelight kvantum-qt5 playerctl libratbag telegram-desktop spotify spotifywm-git bitwarden-bin bc rofi-lbonn-wayland-git jetbrains-toolbox fzf nerd-fonts-iosevka ttf-iosevka-aile insync papirus-icon-theme pulseaudio bluez pavucontrol paprefs android-tools android-udev android-sdk spicetify-cli spicetify-themes-git brightnessctl
 
 # pulseaudio version
-yay -S pulseaudio
+yay --noconfirm -S pulseaudio
 
 # pipewire version
-yay -S pipewire pipewire-pulse pipewire-jack-dropin pipewire-jack pipewire-media-session
+yay --noconfirm -S pipewire pipewire-pulse pipewire-jack-dropin pipewire-jack pipewire-media-session
 
 # bspwm version
-# yay -S bspwm sxhkd picom lxappearance xautomation xcape xdotool xorg-xdpyinfo xorg-input multilockscreen xorg-xrandr xsel xclip xorg-xrdb feh polybar xorg-xsetroot xtitle
+# yay --noconfirm -S bspwm sxhkd picom lxappearance xautomation xcape xdotool xorg-xdpyinfo xorg-input multilockscreen xorg-xrandr xsel xclip xorg-xrdb feh polybar xorg-xsetroot xtitle
 
 # sway version
-yay -S sway rofi-lbonn-wayland-git waybar slurp grim jq swappy
+yay --noconfirm -S sway rofi-lbonn-wayland-git waybar slurp grim jq swappy
