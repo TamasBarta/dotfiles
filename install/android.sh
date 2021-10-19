c0142f1a7b5a611b193499cb7a5d305afebed26a
Tamás Barta <barta.tamas.d@gmail.com>
2021-10-19 18:04:48 +0200

    Android install script

--- install/android.sh
+++ install/android.sh
@@ -1,4 +1,6 @@
 # Install packages
 
-pkg install wget zsh stow git fzf termux-api neofetch nodejs rust python
+pkg install wget zsh stow git fzf termux-api neofetch nodejs rust python golang lsd bat neovim
 
+stow -d $DOTFILES_HOME termux
+stow -d $DOTFILES_HOME neofetch
