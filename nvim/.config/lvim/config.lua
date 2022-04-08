ea956b4006e93442776056cd41846cdd5119ada9
Tamás Barta <barta.tamas.d@gmail.com>
2022-04-09 01:07:17 +0200

    LunarVim config changes

--- nvim/.config/lvim/config.lua
+++ nvim/.config/lvim/config.lua
@@ -75,13 +75,16 @@ lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.git_hl = false
+lvim.builtin.dap.active = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
   "c",
+  "dart",
   "javascript",
   "json",
+  "kotlin",
   "lua",
   "python",
   "typescript",
