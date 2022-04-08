20dd0994a145bd6989d58b2f6af1778f5ecb3912
Tamás Barta <barta.tamas.d@gmail.com>
2022-04-08 22:27:04 +0200

    Use SSH Agent

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -8,3 +8,5 @@ export GVT_DOM="0000:00"
 export GVT_PCI="0000:00:02.0"
 export GVT_GUID="3b0e5f82-d331-4485-82e5-63ccd2efc576"
 export GVT_TYPE="i915-GVTg_V5_4"
+
+if [ -z "${SSH_AUTH_SOCK:+x}" ]; then eval $(ssh-agent); fi
