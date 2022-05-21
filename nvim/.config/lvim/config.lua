64ab9530885fe9be3e3fb7418e6986a3e441f2c0
Tamás Barta <barta.tamas.d@gmail.com>
2022-05-21 20:06:05 +0200

    Migrate LunarVim config to new format

--- nvim/.config/lvim/config.lua
+++ nvim/.config/lvim/config.lua
@@ -12,6 +12,8 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "nightfox"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -105,13 +107,18 @@ lvim.builtin.project.detection_methods = { "lsp" }
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.automatic_servers_installation = false
 
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheReset` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust" })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "dart" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- --`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -158,8 +165,6 @@ lvim.builtin.project.detection_methods = { "lsp" }
 -- }
 
 -- Additional Plugins
-table.insert(lvim.lsp.override, "rust")
-table.insert(lvim.lsp.override, "dart")
 lvim.plugins = {
   {
     "EdenEast/nightfox.nvim",
