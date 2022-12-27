6ee116957b208cad64b024c6451411abf39027b5
Tamás Barta <barta.tamas.d@gmail.com>
2022-12-27 23:31:30 +0100

    Lunarvim chnages

--- nvim/.config/lvim/config.lua
+++ nvim/.config/lvim/config.lua
@@ -117,7 +117,7 @@ lvim.builtin.which_key.mappings["G"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
+-- lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.terminal.open_mapping = '<c-t>'
 lvim.builtin.nvimtree.setup.view.side = "left"
