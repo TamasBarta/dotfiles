8a9193bd5915ee591eff31eb598aa15b513bce01
Tamás Barta <barta.tamas.d@gmail.com>
2023-05-29 02:50:54 +0200

    Add packages to autoinstall

--- install/android.sh
+++ install/android.sh
@@ -1,6 +1,6 @@
 # Install packages
 
-pkg install wget zsh stow git fzf termux-api neofetch nodejs rust python python-pip golang lsd bat neovim jq file vivid
+pkg install wget zsh stow git fzf termux-api neofetch nodejs rust python python-pip golang lsd bat neovim jq file vivid exa
 
 stow -d $DOTFILES_HOME termux
 stow -d $DOTFILES_HOME neofetch
--- install/arch.sh
+++ install/arch.sh
@@ -9,16 +9,10 @@ cd -
 rm -rf /tmp/yay
 
 # common
-yay --noconfirm -S kitty neovim tmux zsh unzip zip stow efibootmgr qt5ct wmname dunst base-devel go adapta-gtk-theme network-manager-applet htop filelight kvantum-qt5 playerctl libratbag telegram-desktop spotify spotifywm-git bitwarden-bin bc rofi-lbonn-wayland-git jetbrains-toolbox fzf nerd-fonts-iosevka ttf-iosevka-aile insync papirus-icon-theme bluez pavucontrol paprefs android-tools android-udev android-sdk spicetify-cli spicetify-themes-git brightnessctl firefox blueman keyd-git
-
-# pulseaudio version
-# yay --noconfirm -S pulseaudio
+yay --noconfirm -S kitty neovim tmux zsh unzip zip stow efibootmgr qt5ct wmname dunst base-devel go adapta-gtk-theme network-manager-applet htop filelight kvantum-qt5 playerctl libratbag telegram-desktop spotify spotifywm-git bitwarden-bin bc rofi-lbonn-wayland-git jetbrains-toolbox fzf nerd-fonts-iosevka ttf-iosevka-aile insync papirus-icon-theme bluez pavucontrol paprefs android-tools android-udev android-sdk spicetify-cli spicetify-themes-git brightnessctl firefox blueman keyd-git exa vivid jq
 
 # pipewire version
 yay --noconfirm -S pipewire pipewire-pulse pipewire-jack-dropin pipewire-jack pipewire-media-session
 
-# bspwm version
-# yay --noconfirm -S bspwm sxhkd picom lxappearance xautomation xdotool xorg-xdpyinfo xorg-input multilockscreen xorg-xrandr xsel xclip xorg-xrdb feh polybar xorg-xsetroot xtitle
-
 # sway version
-yay --noconfirm -S sway rofi-lbonn-wayland-git waybar slurp grim jq swappy wtype wl-clipboard
+yay --noconfirm -S sway rofi-lbonn-wayland-git waybar slurp grim swappy wtype wl-clipboard
