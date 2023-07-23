fe42f567883a12e2ab8d332661c3f87fcc403abd
Tamás Barta <barta.tamas.d@gmail.com>
2023-07-23 23:00:56 +0200

    Update Arch install stuff

--- install/arch.sh
+++ install/arch.sh
@@ -9,7 +9,13 @@ cd -
 rm -rf /tmp/yay
 
 # common
-yay --noconfirm -S kitty neovim tmux zsh unzip zip stow efibootmgr qt5ct wmname dunst base-devel go adapta-gtk-theme network-manager-applet htop filelight kvantum-qt5 playerctl libratbag telegram-desktop spotify spotifywm-git bitwarden-bin bc rofi-lbonn-wayland-git jetbrains-toolbox fzf nerd-fonts-iosevka ttf-iosevka-aile insync papirus-icon-theme bluez pavucontrol paprefs android-tools android-udev android-sdk spicetify-cli spicetify-themes-git brightnessctl firefox blueman keyd-git exa vivid jq
+yay --noconfirm -S neovim tmux zsh unzip zip efibootmgr qt5ct wmname dunst \
+  adapta-gtk-theme network-manager-applet htop filelight kvantum-qt5 playerctl \
+  libratbag telegram-desktop spotify spotifywm-git bitwarden-bin bc \
+  rofi-lbonn-wayland-git jetbrains-toolbox fzf nerd-fonts-iosevka \
+  ttf-iosevka-aile insync papirus-icon-theme bluez pavucontrol paprefs \
+  android-tools android-udev android-sdk spicetify-cli spicetify-themes-git \
+  brightnessctl firefox blueman keyd-git exa vivid jq
 
 # pipewire version
 yay --noconfirm -S pipewire pipewire-pulse pipewire-jack-dropin pipewire-jack pipewire-media-session
