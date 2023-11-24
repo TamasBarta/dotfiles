1ec936f81441ba642bf59ba2b9f3d97ca889501f
Tamás Barta <barta.tamas.d@gmail.com>
2023-11-24 22:39:26 +0100

    Neovim changes

--- nvim/.config/lazyvim/lua/plugins/alpha.lua
+++ nvim/.config/lazyvim/lua/plugins/alpha.lua
@@ -1,29 +1,29 @@
-return {
-  {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
-    opts = function()
-      require("alpha")
-      require("alpha.term")
-      local dashboard = require("alpha.themes.dashboard")
-
-      local width = 40
-      local height = 6
-      dashboard.section.terminal.command = "neo --fps=20 --speed=7 -D -m 'NEOVIM' -d 0.7 -l 1,1"
-      dashboard.section.terminal.width = width
-      dashboard.section.terminal.height = height
-      dashboard.section.terminal.opts.redraw = true
-
-      dashboard.config.layout = {
-        { type = "padding", val = 1 },
-        dashboard.section.terminal,
-        { type = "padding", val = 2 },
-        dashboard.section.buttons,
-        { type = "padding", val = 1 },
-        dashboard.section.footer,
-      }
-
-      return dashboard
-    end,
-  },
-}
+-- return {
+--   {
+--     "goolord/alpha-nvim",
+--     event = "VimEnter",
+--     opts = function()
+--       require("alpha")
+--       require("alpha.term")
+--       local dashboard = require("alpha.themes.dashboard")
+--
+--       local width = 40
+--       local height = 6
+--       dashboard.section.terminal.command = "neo --fps=20 --speed=7 -D -m 'NEOVIM' -d 0.7 -l 1,1"
+--       dashboard.section.terminal.width = width
+--       dashboard.section.terminal.height = height
+--       dashboard.section.terminal.opts.redraw = true
+--
+--       dashboard.config.layout = {
+--         { type = "padding", val = 1 },
+--         dashboard.section.terminal,
+--         { type = "padding", val = 2 },
+--         dashboard.section.buttons,
+--         { type = "padding", val = 1 },
+--         dashboard.section.footer,
+--       }
+--
+--       return dashboard
+--     end,
+--   },
+-- }
