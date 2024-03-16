60bacc202cff887cfcd45645267919a17ac0f728
Tamás Barta <barta.tamas.d@gmail.com>
2024-03-16 19:51:11 +0100

    Update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,25 +10,27 @@ local function scheme_for_appearance(appearance)
 	end
 end
 
+local isLinux = string.find(wezterm.target_triple, "linux")
+
 config.term = "wezterm"
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.font = wezterm.font_with_fallback({ "JetbrainsMono NF", "JetbrainsMono Nerd Font" })
-config.font_size = string.find(wezterm.target_triple, "linux") and 11.5 or 14
-config.line_height = 1.1
+config.font_size = isLinux and 11.5 or 14
+config.line_height = 1.3
 -- config.front_end = "WebGpu"
 config.window_padding = {
 	left = "0cell",
 	right = "0cell",
 	-- top = "0",
-	top = string.find(wezterm.target_triple, "linux") and 11.5 or 56,
+	top = isLinux and 11.5 or 56,
 	bottom = "0.0cell",
 }
 config.enable_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
--- config.window_background_opacity = 0.8
+-- config.window_background_opacity = 0.9
 -- config.macos_window_background_blur = 90
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.integrated_title_button_style = string.find(wezterm.target_triple, "linux") and "Gnome" or "MacOsNative"
+config.integrated_title_button_style = isLinux and "Gnome" or "MacOsNative"
 config.colors = {
 	tab_bar = {
 		-- The color of the inactive tab bar edge/divider
