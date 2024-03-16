e9f9bd0b430586831924da06a9884f3b19120b03
Tamás Barta <barta.tamas.d@gmail.com>
2024-03-17 00:02:30 +0100

    Download proper Hungarian keyboard layout on macOS

--- install/macos.sh
+++ install/macos.sh
@@ -28,6 +28,8 @@ brew tap withgraphite/tap
 brew tap zegervdv/zathura
 # fonts
 brew tap homebrew/cask-fonts
+# keyboardSwitcher
+brew tap lutzifer/homebrew-tap
 
 # install Zathura with macOS patch
 brew install girara --HEAD
@@ -36,11 +38,14 @@ mkdir -p "$(brew --prefix zathura)/lib/zathura"
 ln -s "$(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib" "$(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib"
 
 # Install packages
-brew install zsh ruby stow homeassistant-cli bat tmux neovim lazygit topgrade lf zathura yq jq xq sops scrcpy ripgrep eza dust fd fzf zathura zathura-pdf-poppler yabai skhd borders svim sketchybar buf bundletool chafa coreutils curl wget go gnupg htop kondo just onefetch qrencode shellcheck w3m graphite tunnelto gomuks pinentry-mac
+brew install zsh ruby stow homeassistant-cli bat tmux neovim lazygit topgrade lf zathura yq jq xq sops scrcpy ripgrep eza dust fd fzf zathura zathura-pdf-poppler yabai skhd borders svim sketchybar buf bundletool chafa coreutils curl wget go gnupg htop kondo just onefetch qrencode shellcheck w3m graphite tunnelto gomuks pinentry-mac keyboardswitcher
 
 brew install --cask raycast firefox wezterm element slack spotify obsidian notion calibre signal karabiner-elements bitwarden jetbrains-toolbox todoist hiddenbar betterdisplay figma bluesnooze obs droidcam-obs krita monitorcontrol iina anydesk home-assistant telegram-desktop daisydisk insync onlyoffice minisim tableplus unnaturalscrollwheels min brave-browser gpg-suite
 
 brew install --cask font-jetbrains-mono-nerd-font
+
+# Download proper Hungarian keyboard layout
+wget -O - https://raw.githubusercontent.com/zaki/mac-hun-keyboard/master/Hungarian_Win.keylayout | sudo tee /Library/Keyboard\ Layouts/Hungarian_Win.keylayout
 
 # Create symlinks
 stow -d "$DOTFILES_HOME" karabiner
--- macos/defaults.sh
+++ macos/defaults.sh
@@ -54,10 +54,12 @@ launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2>/dev/null
 # Set language and text formats
 # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
 # `Inches`, `en_GB` with `en_US`, and `true` with `false`.
-defaults write NSGlobalDomain AppleLanguages -array "en" "nl"
+defaults write NSGlobalDomain AppleLanguages -array "en"
 defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=EUR"
 defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
 defaults write NSGlobalDomain AppleMetricUnits -bool true
+# Set selected keyboard layout with keyboardSwitcher
+keyboardSwitcher select HungarianWin
 
 ###############################################################################
 # Energy saving                                                               #
