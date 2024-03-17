34f8f629b79ebff940b7066b19dc096be3771aac
Tamás Barta <barta.tamas.d@gmail.com>
2024-03-17 16:37:33 +0100

    Add macOS settings

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -150,7 +150,7 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 # Enable highlight hover effect for the grid view of a stack (Dock)
 defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
-# Set the icon size of Dock items to 36 pixels
+# Set the icon size of Dock items to 72 pixels
 defaults write com.apple.dock tilesize -int 72
 
 # Change minimize/maximize window effect
@@ -212,8 +212,8 @@ defaults write com.apple.dock dashboard-in-overlay -bool true
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
-# Remove the auto-hiding Dock delay
-defaults write com.apple.dock autohide-delay -float 0
+# Increase the auto-hiding Dock delay, so it never shows unless you need it
+defaults write com.apple.dock autohide-delay -float 10
 # Remove the animation when hiding/showing the Dock
 # defaults write com.apple.dock autohide-time-modifier -float 0
 
