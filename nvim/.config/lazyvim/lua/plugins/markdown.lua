5be0ac887ab1fb00322c8a94349d48462b946b23
Tamás Barta <tamas@bartatamas.hu>
2024-04-11 16:58:39 +0200

    lazyvim configs

--- nvim/.config/lazyvim/lua/plugins/markdown.lua
+++ nvim/.config/lazyvim/lua/plugins/markdown.lua
@@ -1,1 +1,19 @@
-return { "davidgranstrom/nvim-markdown-preview" }
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.api.nvim_exec(
+      [[
+        function OpenMarkdownPreview (url)
+          execute "silent ! open -a Firefox -n --args --new-window " . a:url
+        endfunction
+        let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+      ]],
+      false
+    )
+
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
