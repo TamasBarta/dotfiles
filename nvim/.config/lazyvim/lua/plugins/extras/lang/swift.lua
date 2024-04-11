643a2de52a81eafa11d6a499935f51bea57c3d94
Tamás Barta <tamas@bartatamas.hu>
2024-04-11 16:55:44 +0200

    enable swift formatting in lazyvim

--- nvim/.config/lazyvim/lua/plugins/extras/lang/swift.lua
+++ nvim/.config/lazyvim/lua/plugins/extras/lang/swift.lua
@@ -5,4 +5,15 @@ return {
       opts.servers.sourcekit = {}
     end,
   },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      if opts.sources == nil then
+        opts.sources = {}
+      end
+      table.insert(opts.sources, nls.builtins.formatting.swiftformat)
+      table.insert(opts.sources, nls.builtins.diagnostics.swiftlint)
+    end,
+  },
 }
