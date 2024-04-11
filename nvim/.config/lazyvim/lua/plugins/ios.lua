85f8187853a563f6dab4f09af144761df2f16791
Tamás Barta <tamas@bartatamas.hu>
2024-04-11 16:56:33 +0200

    something with ios in neovmim, but i'm not sure what it's about

---
+++ nvim/.config/lazyvim/lua/plugins/ios.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "xbase-lab/xbase",
+    build = "make install",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-telescope/telescope.nvim", -- optional
+      "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
+      "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+    },
+    config = true,
+  },
+}
