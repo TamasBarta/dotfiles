f20e6fc88857185e42560655f9a411ff481cadf2
Tamás Barta <tamas@bartatamas.hu>
2024-05-24 22:12:24 +0200

    fixes

--- zsh/.config/zsh/path.zsh
+++ zsh/.config/zsh/path.zsh
@@ -7,6 +7,7 @@ export PATH="$PATH:$HOME/.cache/pub/bin"
 export PATH="$PATH:${GOPATH//://bin:}/bin"
 export PATH="$PATH:$CARGO_HOME/bin"
 export PATH="$PATH:$HOME/.spicetify"
+export PATH="$PATH:${XDG_DATA_HOME}/npm/bin"
 
 # Proguard
 export PATH=$PATH:$HOME/Library/Android/sdk/tools/proguard/bin
@@ -17,3 +18,4 @@ export PATH=$PATH:/opt/android-sdk/tools/proguard/bin
 export PATH=$PATH:/opt/android-sdk/platform-tools
 export PATH=$PATH:/opt/android-sdk/tools
 export PATH=$PATH:/opt/android-sdk/tools/bin
+export PATH="/opt/homebrew/Caskroom/android-sdk/4333796/platform-tools/:$PATH"
