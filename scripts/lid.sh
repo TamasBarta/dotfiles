6cdef430fd23d4cb126f39df18a822ea7ed1f3d9
Tamás Barta <tamas@bartatamas.hu>
2024-06-17 10:36:07 +0200

    lid changes

--- scripts/lid.sh
+++ scripts/lid.sh
@@ -7,3 +7,9 @@ if [[ $is_closed -eq 1 ]]; then
 else
   hyprctl keyword monitor "eDP-1,preferred,auto,auto"
 fi
+
+if [ "$1" = "close" ]; then
+  hyprctl keyword monitor "eDP-1,disable"
+elif [ "$1" = "open" ]; then
+  hyprctl keyword monitor "eDP-1,preferred,auto,auto"
+fi
