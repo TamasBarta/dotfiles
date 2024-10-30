e9fd930d16b919b18328ed8ab903f2ecce9f9632
Tamás Barta <tamas@bartatamas.hu>
2024-10-30 01:35:58 +0100

    autostart hyprland

--- zsh/.config/zsh/linux.zsh
+++ zsh/.config/zsh/linux.zsh
@@ -1,3 +1,4 @@
+#!/usr/bin/zsh
 # Linux specific stuff
 
 alias open=xdg-open
@@ -7,19 +8,21 @@ alias open=xdg-open
 case $DESKTOP_SESSION in
     plasma) source $DOTFILES_HOME/zsh/.config/zsh/kde.zsh ;;
     /usr/share/xsessions/plasma) source $DOTFILES_HOME/zsh/.config/zsh/kde.zsh ;;
-    *)
+    *) ;;
 esac
 
 if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
-        source /etc/profile.d/vte.sh
+    source /etc/profile.d/vte.sh
 fi
 
 _systemctl_unit_state() {
   typeset -gA _sys_unit_state
-  _sys_unit_state=( $(__systemctl list-unit-files "$PREFIX*" | awk '{print $1, $2}') ) }
+  _sys_unit_state=( $(__systemctl list-unit-files "$PREFIX*" | awk '{print $1, $2}') ) 
+}
 
 
 if [ -z $DISPLAY ] && [[ "$(tty)" == "/dev/tty1" ]]; then
+  Hyprland
   # exec sway --my-next-gpu-wont-be-nvidia
   # exec sway --unsupported-gpu
 fi
