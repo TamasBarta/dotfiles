bd345a30305b8f2182ac3caf738bd113a6937d13
Tamás Barta <tamas@bartatamas.hu>
2024-10-30 01:17:33 +0100

    fix rofi install

--- install/arch.sh
+++ install/arch.sh
@@ -9,16 +9,16 @@ cd -
 rm -rf /tmp/yay-bin
 
 # common
-yay --noconfirm -S neovim tmux zsh unzip zip efibootmgr wmname dunst \
+yay --needed --noconfirm -S neovim tmux zsh unzip zip efibootmgr wmname dunst \
 	network-manager-applet htop filelight playerctl \
 	telegram-desktop spotify spotifywm-git bitwarden-bin bc \
-	rofi-lbonn-wayland-git jetbrains-toolbox nerd-fonts-iosevka \
+	jetbrains-toolbox nerd-fonts-iosevka \
 	ttf-iosevka-aile insync bluez pavucontrol paprefs \
 	android-tools android-udev android-sdk spicetify-cli spicetify-themes-git \
 	brightnessctl firefox blueman keyd-git exa jq go fzf xxd
 
 # pipewire version
-yay --noconfirm -S pipewire pipewire-pulse pipewire-jack-dropin pipewire-jack pipewire-media-session
+yay --needed --noconfirm -S pipewire pipewire-pulse pipewire-jack-dropin pipewire-jack pipewire-media-session
 
 # Hyprland version
-yay --noconfirm -S rofi-wayland waybar slurp grim swappy wtype wl-clipboard hyprland hy3
+yay --needed --noconfirm -S rofi-wayland waybar slurp grim swappy wtype wl-clipboard hyprland hy3
