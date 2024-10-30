170474b1290554799cbc8c4700771ab3e1c129e5
Tamás Barta <tamas@bartatamas.hu>
2024-10-30 01:03:41 +0100

    remove a lot more unused configs

--- lf/.config/lf/scope-lf-wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. preview
-"$HOME/.config/ranger/scope.sh" "${1}" "${2}" "${3}" "" "" || true
--- nvim/.config/astronvim/lua/user/plugins/treesitter.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "lua", "dart", "toml", "rust" },
-    auto_install = true,
-  },
-}
