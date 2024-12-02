1ef0f9f1a35f81702674f798f6c3e4c17acc1a42
Tamás Barta <tamas@bartatamas.hu>
2024-12-02 20:15:43 +0100

    linux "pwa" script changes

--- scripts/pwa.sh
+++ scripts/pwa.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-chromium-browser --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer \
+# See if Chromium is available as chromium of chromium-browser command
+
+if [ -x "$(command -v chromium)" ]; then
+	browser="chromium"
+elif [ -x "$(command -v chromium-browser)" ]; then
+	browser="chromium-browser"
+else
+	echo "Chromium is not installed"
+	exit 1
+fi
+
+$browser --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer \
 	--ozone-platform=wayland \
 	--app="$1"
