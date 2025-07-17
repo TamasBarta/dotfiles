95b1772d19f68a0c9f3c17c84a12a4424910e0f5
Tamás Barta <tamas@bartatamas.hu>
2025-07-17 16:55:44 +0200

    yazi

--- yazi/.config/yazi/plugins/mactag.yazi/main.lua
+++ yazi/.config/yazi/plugins/mactag.yazi/main.lua
@@ -1,8 +1,15 @@
+--- @since 25.5.31
+
 local update = ya.sync(function(st, tags)
 	for path, tag in pairs(tags) do
 		st.tags[path] = #tag > 0 and tag or nil
 	end
-	ya.render()
+	-- TODO: remove this
+	if ui.render then
+		ui.render()
+	else
+		ya.render()
+	end
 end)
 
 local selected_or_hovered = ya.sync(function()
@@ -25,7 +32,7 @@ local function setup(st, opts)
 		local url = tostring(self._file.url)
 		local spans = {}
 		for _, tag in ipairs(st.tags[url] or {}) do
-			if self._file:is_hovered() then
+			if self._file.is_hovered then
 				spans[#spans + 1] = ui.Span(" ●"):bg(st.colors[tag] or "reset")
 			else
 				spans[#spans + 1] = ui.Span(" ●"):fg(st.colors[tag] or "reset")
@@ -35,18 +42,15 @@ local function setup(st, opts)
 	end, 500)
 end
 
-local function fetch(self, job)
-	-- TODO: remove this once Yazi 0.4 is released
-	job = job or self
-
+local function fetch(_, job)
 	local paths = {}
 	for _, file in ipairs(job.files) do
 		paths[#paths + 1] = tostring(file.url)
 	end
 
-	local output, err = Command("tag"):args(paths):stdout(Command.PIPED):output()
+	local output, err = Command("tag"):arg(paths):stdout(Command.PIPED):output()
 	if not output then
-		return ya.err("Cannot spawn tag command, error code " .. tostring(err))
+		return true, Err("Cannot spawn `tag` command, error: %s", err)
 	end
 
 	local i, tags = 1, {}
@@ -64,7 +68,7 @@ local function fetch(self, job)
 	end
 
 	update(tags)
-	return 1
+	return true
 end
 
 local cands = ya.sync(function(st)
@@ -76,11 +80,8 @@ local cands = ya.sync(function(st)
 end)
 
 local function entry(self, job)
-	-- TODO: remove this once Yazi 0.4 is released
-	local args = job.args or job
-
-	assert(args[1] == "add" or args[1] == "remove", "Invalid action")
-	ya.manager_emit("escape", { visual = true })
+	assert(job.args[1] == "add" or job.args[1] == "remove", "Invalid action")
+	ya.emit("escape", { visual = true })
 
 	local cands = cands()
 	local choice = ya.which { cands = cands }
@@ -88,14 +89,14 @@ local function entry(self, job)
 		return
 	end
 
-	local t = { args[1] == "remove" and "-r" or "-a", cands[choice].desc }
+	local t = { job.args[1] == "remove" and "-r" or "-a", cands[choice].desc }
 	local files = {}
 	for _, url in ipairs(selected_or_hovered()) do
 		t[#t + 1] = tostring(url)
 		files[#files + 1] = { url = url }
 	end
 
-	local status = Command("tag"):args(t):status()
+	local status = Command("tag"):arg(t):status()
 	if status.success then
 		fetch(self, { files = files })
 	end
