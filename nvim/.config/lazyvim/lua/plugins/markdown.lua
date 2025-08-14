b5424fd0dfceb4c6ec17a335fbd72a67933ede50
Tamás Barta <tamas@bartatamas.hu>
2025-08-15 00:36:03 +0200

    change markdown settings in neovim

--- nvim/.config/lazyvim/lua/plugins/markdown.lua
+++ nvim/.config/lazyvim/lua/plugins/markdown.lua
@@ -1,19 +1,8 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
-  init = function()
-    vim.api.nvim_exec(
-      [[
-        function OpenMarkdownPreview (url)
-          execute "silent ! open -a Firefox -n --args --new-window " . a:url
-        endfunction
-        let g:mkdp_browserfunc = 'OpenMarkdownPreview'
-      ]],
-      false
-    )
-
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = function(_, _)
+      return {}
+    end,
+  },
 }
