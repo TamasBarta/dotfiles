d5e1e19eafa9daa741c01a43a5e3d2cbd3e78216
Tamás Barta <tamas@bartatamas.hu>
2026-05-06 01:46:28 +0200

    opencode in nvim

---
+++ nvim/.config/lazyvim/lua/plugins/extras/opencode.lua
@@ -0,0 +1,83 @@
+return {
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>a", group = "ai" },
+      },
+    },
+  },
+  {
+    "NickvanDyke/opencode.nvim",
+    dependencies = {
+      -- Recommended for `ask()` and `select()`.
+      -- Required for `snacks` provider.
+      ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    },
+    keys = {
+      {
+        mode = { "n", "x" },
+        "<leader>aa",
+        function()
+          require("opencode").ask("@this: ", { submit = true })
+        end,
+        desc = "Ask opencode…",
+      },
+      {
+        mode = { "n", "x" },
+        "<leader>ax",
+        function()
+          require("opencode").select()
+        end,
+        desc = "Execute opencode action…",
+      },
+      {
+        mode = { "n", "x" },
+        "<leader>at",
+        function()
+          require("opencode").toggle()
+        end,
+        desc = "Toggle opencode",
+      },
+      {
+        "go",
+        function()
+          return require("opencode").operator("@this ")
+        end,
+        desc = "Add range to opencode",
+      },
+      {
+        mode = { "n", "x" },
+        "goo",
+        function()
+          return require("opencode").operator("@this ") .. "_"
+        end,
+        desc = "Add line to opencode",
+      },
+      {
+        "<S-C-u>",
+        function()
+          require("opencode").command("session.half.page.up")
+        end,
+        desc = "Scroll opencode up",
+      },
+      {
+        "<S-C-d>",
+        function()
+          require("opencode").command("session.half.page.down")
+        end,
+        desc = "Scroll opencode down",
+      },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
+      }
+      -- Required for `opts.events.reload`.
+      vim.o.autoread = true
+    end,
+  },
+}
