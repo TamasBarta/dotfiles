6598090ffd45d5968d3f72d5f94be2b4067fdd6b
Tamás Barta <tamas@bartatamas.hu>
2026-06-30 15:28:42 +0200

    termux packages

--- install/android.sh
+++ install/android.sh
@@ -1,6 +1,6 @@
 # Install packages
 
-pkg install wget zsh stow git fzf termux-api neofetch nodejs rust python python-pip golang lsd bat neovim jq file vivid exa
+pkg install wget zsh stow git fzf termux-api neofetch nodejs rust python python-pip golang lsd bat neovim jq file vivid eza git-crypt lazygit
 
 stow -d $DOTFILES_HOME termux
 stow -d $DOTFILES_HOME neofetch
