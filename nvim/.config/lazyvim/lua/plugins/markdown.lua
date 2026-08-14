1dcf82208257f961a6b158a07074027c41d1f266
Tamás Barta <tamas@bartatamas.hu>
2026-08-14 18:58:14 +0200

    neovim fixes

--- nvim/.config/lazyvim/lua/plugins/markdown.lua
+++ nvim/.config/lazyvim/lua/plugins/markdown.lua
@@ -5,20 +5,4 @@ return {
       return {}
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    -- -- This breaks keymaps for lsp
-    -- config = function()
-    --   vim.api.nvim_create_autocmd("FileType", {
-    --     pattern = "markdown",
-    --     callback = function()
-    --       vim.diagnostic.config({
-    --         virtual_text = false,
-    --         underline = false,
-    --         signs = false,
-    --       }, vim.api.nvim_get_current_buf())
-    --     end,
-    --   })
-    -- end,
-  },
 }
